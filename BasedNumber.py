15559e67fba6d5f3b4f8b939f62e63f4719a1fff
kk7k3r <d.ostrozhnoy@gmail.com>
2025-05-07 21:02:24 +0500

    BasedNumber

---
+++ BasedNumber.py
@@ -0,0 +1,33 @@
+﻿class BasedNumber:
+    def __init__(self, value, base):
+        self.value = value
+        self.base = base
+
+    def add(self, other):
+        val1, val2 = self.convertValues(other)
+        return BasedNumber(val1 + val2, 10)
+
+    def sub(self, other):
+        val1, val2 = self.convertValues(other)
+        return BasedNumber(val1 - val2, 10)
+
+    def mult(self, other):
+        val1, val2 = self.convertValues(other)
+        return BasedNumber(val1 * val2, 10)
+
+    def div(self, other):
+        val1, val2 = self.convertValues(other)
+        if val2 == 0:
+            raise ZeroDivisionError("Деление на ноль невозможно.")
+        return BasedNumber(val1 / val2, 10)
+
+    def convertValues(self, other):
+        return [self.toDecimal(), other.toDecimal()]
+
+    def toDecimal(self):
+        if self.base != 10 or isinstance(self.value, str):
+            return int(self.value), int(self.base)
+        return self.value
+
+    def __str__(self):
+        return f"{self.value} (base {self.base})"
