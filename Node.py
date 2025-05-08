ad9a7ef9b668e3bd885a382ada39a54db95e86c2
kk7k3r <d.ostrozhnoy@gmail.com>
2025-05-08 19:09:08 +0500

    Random expression generator

---
+++ Node.py
@@ -0,0 +1,6 @@
+﻿class Node:
+    """Класс для представления узла дерева"""
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
---
+++ RandomExpressionGenerator.py
@@ -0,0 +1,43 @@
+﻿import random
+import Node
+from BasedNumber import BasedNumber
+
+class RandomExpressionGenerator:
+    def __init__(self):
+        self.operations = ['+', '-', '*', '/']
+        self.max_depth = 5  # Максимальная глубина дерева
+
+    def getRandomOperation(self):
+        """Получение случайной операции"""
+        return random.choice(self.operations)
+
+    def getRandomNumber(self):
+        """Получение случайного числа"""
+        base = random.randint(2, 10)
+        num = random.randint(-100, 100)
+        value = format(num if num >= 0 else (1 << 8) + num, f'0{base}').lower();
+        return BasedNumber(value, base)
+
+    def choice(self, seq):
+        """Функция для выбора случайного элемента из списка"""
+        return random.choice(seq)
+
+    def build_subtree(self, depth, stack):
+        child = Node(None)
+        if random.choice(['operation', 'number']) == 'operation' and depth <= self.max_depth:
+            child.value = self.get_random_operation()
+            stack.append((child, depth + 1))
+        else:
+            child.value = self.get_random_number()
+        return child
+
+    def create_tree(self):
+        root = Node(self.get_random_operation())
+        stack = [(root, 1)]
+
+        while stack:
+            node, depth = stack.pop()
+            node.left = self.build_subtree(depth, stack)
+            node.right = self.build_subtree(depth, stack)
+
+        return root
